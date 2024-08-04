2a96f198d6f88207c2e6ddfc48af15f3c62e089c
Batman <Batmanaman@connect.ust.hk>
2024-08-04 18:03:30 +0800

    the web app is ready

--- House_pricing/app.py
+++ House_pricing/app.py
@@ -8,9 +8,11 @@ app=Flask(__name__)
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 scaler = pickle.load(open('scaling.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -22,5 +24,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The predicted House price is {}".format(output))
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
